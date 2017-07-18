fa186b091ff07ee3fabaa09961273f8d9118de39
penglei <yupengniaosanshi@gmail.com>
2017-07-18 15:24:01 +0800

    在测试中添加了自定义委托

--- weatherapp/src/test/java/com/example/weatherapp/ExampleUnitTest.kt
+++ weatherapp/src/test/java/com/example/weatherapp/ExampleUnitTest.kt
@@ -3,6 +3,8 @@ package com.example.weatherapp
 import org.junit.Test
 
 import org.junit.Assert.*
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +15,33 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    /**
+     * 自定义委托
+     */
+    private class NotNullSingleValue<T> : ReadWriteProperty<Any?, T> {
+        private var value: T? = null
+        override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+            return value ?: throw IllegalStateException("${property.name} not initialized")
+        }
+
+        override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+            this.value = if (this.value == null) value else
+                throw IllegalStateException("${property.name} already initialized")
+        }
+    }
+
+    private object DelegatesExt {
+        fun <T> notNullSingleValue() = NotNullSingleValue<T>()
+    }
+
+    @Test
+    fun test() {
+//        var name by DelegatesExt.notNullSingleValue<String>()
+        var name by NotNullSingleValue<String>()
+        name = "Hello"
+        println(name)
+        name = "world"
     }
 }
