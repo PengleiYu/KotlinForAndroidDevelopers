c615b14cd1050278e3c4a812c7e102f22b860594
penglei <yupengniaosanshi@gmail.com>
2017-07-18 15:06:36 +0800

    App使用notNull委托

--- weatherapp/src/main/java/com/example/weatherapp/ui/App.kt
+++ weatherapp/src/main/java/com/example/weatherapp/ui/App.kt
@@ -1,19 +1,21 @@
 package com.example.weatherapp.ui
 
 import android.app.Application
+import kotlin.properties.Delegates
 
 /**
  * Created by yupenglei on 17/7/18.
  */
 
-class App:Application(){
-    companion object{
-        private var instance:Application?=null
-        fun instance()= instance!!
+class App : Application() {
+    companion object {
+        //        private var instance:Application?=null
+//        fun instance()= instance!!
+        var instance: App by Delegates.notNull<App>()
     }
 
     override fun onCreate() {
         super.onCreate()
-        instance=this
+        instance = this
     }
 }
