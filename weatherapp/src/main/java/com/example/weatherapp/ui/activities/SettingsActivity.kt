83055553d0fe786832f70990d69a374bf1130e7c
unknown <>
2017-07-29 15:59:47 +0800

    SettingsActivity可修改zipCode，通过将其委托来简化读取和存储

--- weatherapp/src/main/java/com/example/weatherapp/ui/activities/SettingsActivity.kt
+++ weatherapp/src/main/java/com/example/weatherapp/ui/activities/SettingsActivity.kt
@@ -3,17 +3,33 @@ package com.example.weatherapp.ui.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.weatherapp.R
+import com.example.weatherapp.extensions.DelegatesExt
+import kotlinx.android.synthetic.main.activity_settings.*
 import kotlinx.android.synthetic.main.toolbar.*
 
 /**
  * Created by JJBOOM on 2017/7/29.
  */
-class SettingsActivity : AppCompatActivity(){
+class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        cityCode.setText(zipCode.toString())
     }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        zipCode = cityCode.text.toString().toLong()
+    }
+
+    companion object {
+        val ZIP_CODE = "zipCode"
+        val DEFAULT_ZIP = 94043L
+    }
+
+    //委托
+    var zipCode: Long by DelegatesExt.longPreference(this, ZIP_CODE, DEFAULT_ZIP)
 }
